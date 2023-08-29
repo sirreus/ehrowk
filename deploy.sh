46b237ffaf160e9802e7e62e1faf0aded689a7ac
Kirill Egorov <kirill.egorov@x5.ru>
2023-08-29 19:06:00 +0200

    fixed deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,4 @@ npm run build
 git add dist && git commit -m "subtree deployment commit"
 
 # deploy
-# git subtree push --prefix dist origin gh-pages
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
